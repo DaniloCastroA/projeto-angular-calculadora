1ed9bd4ce468844593611074f6cd276b232a8cc0
DaniloCastroA <danilocastro18ordem@gmail.com>
2024-10-22 20:47:52 -0300

    Adicionando lisa generica para teste

--- calculadora/src/app/modulo-novo/modulo-novo.component.ts
+++ calculadora/src/app/modulo-novo/modulo-novo.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modulo-novo',
   templateUrl: './modulo-novo.component.html',
   styleUrl: './modulo-novo.component.css'
 })
-export class ModuloNovoComponent {
+export class ModuloNovoComponent implements OnInit {
 
+  nomePortal: string;
+  cursos: string[]=['Cadeira','Vasssoura','Arroz'];
+
+  constructor(){
+    this.nomePortal='objetos:';
+
+    for(let i=0; i<this.cursos.length; ++i){
+      let curso = this.cursos[i];
+    }
+  }
+  ngOnInit(): void {
+    
+  }
 }
