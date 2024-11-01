89c1943c89b0646bf50450efb30a294a678b3eb4
DaniloCastroA <danilocastro18ordem@gmail.com>
2024-10-31 21:38:29 -0300

    Inserindo caso onde o primeiro input subtitui o 0 default do label

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
+
+  contador: number = 0;
+
+  valorDigitado: string = "0";
+
+  botaoClicado(value: string): void {
+    if (this.contador == 0){
+      this.valorDigitado=value;
+      this.contador=1;
+    }
+    else{
+      this.valorDigitado += value; 
+    }
+  }
 }
--- calculadora/src/app/botoes-calculadora/botoes-calculadora.component.ts
+++ calculadora/src/app/botoes-calculadora/botoes-calculadora.component.ts
@@ -7,14 +7,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class BotoesCalculadoraComponent {
   @Input() value: string = '';
-  @Input() width: number = 80;
-  @Input() margin: number = 10;
-  @Input() height: number = 40;
-  @Input() fontWeight: string = 'bold';
-  @Input() fontSize: number = 35;
 
   @Output() clickEvent = new EventEmitter<string>();
-
+  
   onClick(): void {
     this.clickEvent.emit(this.value);
   }
