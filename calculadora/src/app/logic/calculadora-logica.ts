a4ec175b4ac572627b5bf33da7328c6622664cac
André Luiz Clinio <clinio@tecgraf.puc-rio.br>
2024-11-07 14:08:26 -0300

    testes clinio

---
+++ calculadora/src/app/logic/calculadora-logica.ts
@@ -0,0 +1,15 @@
+
+
+class CalculadoraLogica {
+
+  private static visor: string = "0";
+
+  public static pegaConteudoVisor(): string {
+    return CalculadoraLogica.visor;
+  }
+
+  public static entraTecla(key: string) {
+
+  }
+
+}
