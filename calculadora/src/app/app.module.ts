ab09890324a2e75dca584009a8cfd4785377376f
danilocastro <danilocastro@tecgraf.puc-rio.br>
2024-12-03 19:40:26 -0300

    Adicionando o app component na pasta component

--- calculadora/src/app/app.module.ts
+++ calculadora/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './component/app_component/app.component';
 import { BotoesCalculadoraComponent } from './component/botoes-calculadora/botoes-calculadora.component';
 import { VisorCalculadoraComponent } from './component/visor-calculadora/visor-calculadora.component';
 
